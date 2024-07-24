9e1492863ee0e928675f993f8565c9b01ddb64d2
James McHugh <jdmchugh111@gmail.com>
2024-07-23 21:20:09 -0400

    still working out Faraday connection

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::PropertiesController < ApplicationController
   def index
     facade = PropertiesFacade.new
-    facade.oompr_search(params[:city])
+    properties = facade.oompr_search(params[:city])
+    render json: properties
   end
 end
--- app/services/properties_service.rb
+++ app/services/properties_service.rb
@@ -1,17 +1,17 @@
 class PropertiesService
   def conn
     conn = Faraday.new(url: "https://api.simplyrets.com") do |faraday|
-      faraday.headers[:Authorization] = "Basic c2ltcGx5cmV0czpzaW1wbHlyZXRz"
+      faraday.basic_auth("simplyrets", "simplyrets")
     end
   end
   
   def get_url(url)
     response = conn.get(url)
+    data = JSON.parse(response.body, symbolize_names: true)
     binding.pry
-    JSON.parse(response.body, symbolize_names: true)
   end
 
   def get_listings(city)
-    get_url("?cities=#{city}&limit=50&sort=-listprice")
+    get_url("?city=#{city}&limit=50&sort=-listprice")
   end
 end
